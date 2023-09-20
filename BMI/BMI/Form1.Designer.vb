4780ee0999aaac641f6fee6a48fd0cf621b864b5
TalaveraBryan <bryanjaytalavera22@gmail.com>
2023-09-20 11:53:33 +0800

    BMI

---
+++ BMI/BMI/Form1.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.KG = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(21, 126)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Height:"
+        Me.Label1.UseWaitCursor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(21, 86)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Weight:"
+        Me.Label2.UseWaitCursor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(21, 172)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "BMI:"
+        Me.Label3.UseWaitCursor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(21, 218)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(179, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "BMI CATEGORY:"
+        Me.Label4.UseWaitCursor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.AllowDrop = True
+        Me.TextBox1.CausesValidation = False
+        Me.TextBox1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.TextBox1.ForeColor = System.Drawing.Color.Gray
+        Me.TextBox1.ImeMode = System.Windows.Forms.ImeMode.Hiragana
+        Me.TextBox1.Location = New System.Drawing.Point(194, 86)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal
+        Me.TextBox1.Size = New System.Drawing.Size(118, 25)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.TabStop = False
+        Me.TextBox1.Text = "KG"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox2.ForeColor = System.Drawing.Color.Gray
+        Me.TextBox2.HideSelection = False
+        Me.TextBox2.Location = New System.Drawing.Point(194, 132)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(118, 25)
+        Me.TextBox2.TabIndex = 5
+        Me.TextBox2.TabStop = False
+        Me.TextBox2.Text = "CM"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Location = New System.Drawing.Point(194, 178)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(118, 26)
+        Me.TextBox3.TabIndex = 6
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.TextBox3.UseWaitCursor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox4.Enabled = False
+        Me.TextBox4.Location = New System.Drawing.Point(194, 218)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.Size = New System.Drawing.Size(118, 33)
+        Me.TextBox4.TabIndex = 7
+        Me.TextBox4.TabStop = False
+        Me.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.TextBox4.UseWaitCursor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 280)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(107, 48)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Clear"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.UseWaitCursor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(141, 280)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(129, 48)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Compute"
+        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.UseWaitCursor = True
+        '
+        'KG
+        '
+        Me.KG.AutoSize = True
+        Me.KG.BackColor = System.Drawing.Color.Transparent
+        Me.KG.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.KG.Location = New System.Drawing.Point(312, 86)
+        Me.KG.Name = "KG"
+        Me.KG.Size = New System.Drawing.Size(42, 25)
+        Me.KG.TabIndex = 10
+        Me.KG.Text = "KG"
+        Me.KG.UseWaitCursor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Transparent
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(312, 132)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(45, 25)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "CM"
+        Me.Label6.UseWaitCursor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.BMI.My.Resources.Resources.download
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(506, 526)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.KG)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "BMI"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents KG As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ BMI/BMI/Form1.vb
@@ -0,0 +1,96 @@
+﻿Public Class Form1
+    Private Sub Label5_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim BMI As Double
+        Dim meter As Double
+        Dim weight As Double
+        Dim height As Double
+        weight = TextBox1.Text
+        height = TextBox2.Text
+
+
+        meter = height / 100
+        BMI = weight / (meter * meter)
+
+        TextBox3.Text = Math.Round(BMI, 2)
+
+        If BMI < 15 Then
+            TextBox4.Text = "Very severely underweight"
+
+        ElseIf BMI >= 15 And BMI <= 16 Then
+            TextBox4.Text = "Severely underweight"
+        ElseIf BMI >= 15 And BMI <= 25 Then
+            TextBox4.Text = "Underweight"
+        ElseIf BMI >= 25 And BMI <= 30 Then
+            TextBox4.Text = "Normal (Healthy Weight)"
+        ElseIf BMI >= 30 And BMI <= 35 Then
+            TextBox4.Text = "Overweight"
+        ElseIf BMI >= 30 And BMI <= 35 Then
+            TextBox4.Text = "Moderately Obese"
+        ElseIf BMI >= 35 And BMI <= 40 Then
+            TextBox4.Text = "Severely Obese"
+        ElseIf BMI > 40 Then
+            TextBox4.Text = "Very Severely Obese"
+
+
+        End If
+    End Sub
+
+
+
+    Private Sub TextBox1_GotFocus(sender As Object, e As EventArgs) Handles TextBox1.GotFocus
+
+        If TextBox1.Text = "KG" Then
+            TextBox1.Clear()
+            TextBox1.ForeColor = Color.Black
+        End If
+
+    End Sub
+
+    Private Sub TextBox2_GotFocus(sender As Object, e As EventArgs) Handles TextBox2.GotFocus
+        If TextBox2.Text = "CM" Then
+            TextBox2.Clear()
+            TextBox2.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub TextBox1_LostFocus(sender As Object, e As EventArgs) Handles TextBox1.LostFocus
+        If TextBox1.Text = "" Then
+            TextBox1.Text = "KG"
+            TextBox1.ForeColor = Color.Gray
+        End If
+    End Sub
+
+    Private Sub TextBox2_LostFocus(sender As Object, e As EventArgs) Handles TextBox2.LostFocus
+        If TextBox2.Text = "" Then
+            TextBox2.Text = "CM"
+            TextBox2.ForeColor = Color.Gray
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For Each i As TextBox In Me.Controls.OfType(Of TextBox)()
+            i.Clear()
+        Next
+        TextBox1.Text = "KG"
+        TextBox1.ForeColor = Color.Gray
+        TextBox2.Text = "CM"
+        TextBox2.ForeColor = Color.Gray
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+End Class
